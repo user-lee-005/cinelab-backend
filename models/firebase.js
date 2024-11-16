ed5b15f899fd7f26c41364a525d520bf1840d121
user-lee-005 <93381862+user-lee-005@users.noreply.github.com>
2024-11-16 16:24:45 +0530

    Changed the import statements of firebase

--- models/firebase.js
+++ models/firebase.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection } from "firebase/firestore";
+const { initializeApp } = require("firebase/app");
+const { getFirestore, collection } = require("firebase/firestore");
 
 const firebaseConfig = {
   apiKey: "AIzaSyAFeyxHhfmnqCg7G4aP-SMH4Zl_tZcy7T8",
